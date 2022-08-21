38e0cb51bc8ceff7340e3feaf81e03f7b0564ad7
Ayush <ayushks070707@gmail.com>
2022-08-21 14:08:40 +0530

    added and edited

---
+++ HVxAyush/JSfiles_for00/file42.js
@@ -0,0 +1,21 @@
+// objects inside array
+// very useful
+
+const users = [
+    {
+        userID: 1,
+        name: "Ayush",
+        gender: "Male",
+    },
+    {
+        userID: 2,
+        name: "Harshit",
+        gender: "Male",
+    },
+    {
+        userID: 3,
+        name: "Amanda",
+        gender: "Female",
+    },
+]
+console.log(users);
