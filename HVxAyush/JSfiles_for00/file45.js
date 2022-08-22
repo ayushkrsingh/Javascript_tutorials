d3918f100194615eab598debb58fd23cdc37be41
Ayush <ayushks070707@gmail.com>
2022-08-22 18:56:29 +0530

    Done file45.js

--- HVxAyush/JSfiles_for00/file45.js
+++ HVxAyush/JSfiles_for00/file45.js
@@ -3,42 +3,56 @@
 // 5:12:00
 
 
+// There will bw no difference !!!!!!!!!!
 
-
-
-
-
-function singAsong(){
+// function singAsong(){        //#### FUNCTION DECLARATION
+//     console.log("Happy Badday to YOUUU !!!!!!!");
+// }
+const singAsong = function(){   //#### FUNCTION EXPRESSION
     console.log("Happy Badday to YOUUU !!!!!!!");
 }
+singAsong();
 
 
-function threePLUSnine(){
-    return 3+9;
-}
-
-
-function sumTWOnums(num1, num2){
-    return num1+num2;
-}
-
-
-function sumTHREEnums(num1, num2, num3){
+// function sumTHREEnums(num1, num2, num3){
+//     return num1+num2+num3;
+// }
+const sumTHREEnums = function(num1, num2, num3){
     return num1+num2+num3;
 }
+console.log(sumTHREEnums(2, 54, 4));
 
 
-function isEven(yourNUM){
+
+// function isEven(yourNUM){
+//     return yourNUM % 2 === 0;
+// }
+const isEven = function(yourNUM) {
     return yourNUM % 2 === 0;
 }
+console.log(isEven(23));
 
 
-function giveFirstChar(yourString){
+
+// function giveFirstChar(yourString){
+//     return yourString[0];
+// }
+const giveFirstChar = function(yourString){
     return yourString[0];
 }
+console.log(giveFirstChar("TomBhai"));
 
 
-function findtarget(MyArray, MyTarget){
+
+// function findtarget(MyArray, MyTarget){
+//     for (let i=0; i<MyArray.length; i++) {
+//         if (MyArray[i] === MyTarget) {
+//             return i;
+//         }
+//     }
+//     return false;
+// }
+const findtarget = function(MyArray, MyTarget){
     for (let i=0; i<MyArray.length; i++) {
         if (MyArray[i] === MyTarget) {
             return i;
@@ -46,3 +60,4 @@ function findtarget(MyArray, MyTarget){
     }
     return false;
 }
+console.log(findtarget([23,2, 5, 7, 19, 45], 5));
