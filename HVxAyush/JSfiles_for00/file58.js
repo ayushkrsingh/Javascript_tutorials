c2cf0345e19164e64a6f487be1e03c2910937f81
Ayush <ayushks070707@gmail.com>
2022-08-27 17:51:34 +0530

    Done till file57.js

---
+++ HVxAyush/JSfiles_for00/file58.js
@@ -0,0 +1,16 @@
+// Filter Method
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+
+// const isEven = function(number){
+//     return number%2===0;       ///  Should always return a boolean type
+// }
+// const evennums = numbers.filter(isEven);
+
+// %%%%%   OR
+
+const evenNums = numbers.filter((number)=>{
+    return number%2===0;
+})
+
+console.log(evenNums);
